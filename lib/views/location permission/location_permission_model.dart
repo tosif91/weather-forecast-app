b7b2a5ef85716b639f1f7176e270de0cde076b5c
tosif91 <61594032+tosif91@users.noreply.github.com>
2021-04-08 16:42:13 +0530

    fix

--- lib/views/location permission/location_permission_model.dart
+++ lib/views/location permission/location_permission_model.dart
@@ -2,17 +2,26 @@ import 'package:stacked/stacked.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:weather_app/locator.dart';
+import 'package:weather_app/services/authentication_service.dart';
 import 'package:weather_app/utils/routes.dart';
 
 class LocationPermissionModel extends BaseViewModel {
   SnackbarService _snackbarService = locator<SnackbarService>();
   NavigationService _navigationService = locator<NavigationService>();
+  AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
   checkLocationPermission() {
     // setBusy(true);
     Permission.location.request().then((status) {
       switch (status) {
         case PermissionStatus.granted:
-          _navigationService.clearStackAndShow(home);
+          {
+            bool status = _authenticationService.checkUser();
+            if (status)
+              _navigationService.clearStackAndShow(home);
+            else
+              _navigationService.clearStackAndShow(logIn);
+          }
           break;
         default:
           _snackbarService.showSnackbar(
