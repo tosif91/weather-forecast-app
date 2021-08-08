6f86869acee60edb7fd0cc64d1c99965d50b9a60
tosif91 <61594032+tosif91@users.noreply.github.com>
2021-08-08 17:06:49 +0530

    api removed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
-const String weatherApiKey = 'e91f6b6f9fc40e8101e7bcefe32fbf42';
+const String weatherApiKey = 'your api key here';
 const String baseURL = 'https://api.openweathermap.org/data/2.5/';
 
 const String phoneNo = 'phoneNumber';
